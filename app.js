3c8325d5aa39360a73c53fa8bd846ce479c5e780
Sikandar Ali <sikandar.6a@gmail.com>
2023-11-22 22:41:32 +0500

    fixed login and register APIs

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express= require('express');
+const bcrypt = require('bcrypt');
 
 const app=express();
 app.use(express.json());
@@ -8,13 +9,12 @@ const db = require('./db');
 app.post("/register", async (req, res) => {
     try {
         const { username, email, password, name, contact } = req.body;
-
-        // Hash the password before storing it in the database
-    const hashedPassword = await bcrypt.hash(password, 10);
-
+    // Hash the password before storing it in the database
+    const saltRounds = 10;
+    const hashedPassword = await bcrypt.hash(password, saltRounds);
         const newUser = await db.query(
             "INSERT INTO users (username, email, password, name, contact) VALUES (?, ?, ?, ?, ?)",
-            [username, email, password, name, contact]
+            [username, email, hashedPassword, name, contact]
         );
         res.status(200).send("User added successfully"); // Assuming the response structure here
     } catch (error) {
@@ -27,22 +27,32 @@ app.post("/login", async (req, res) => {
     try {
         const { username, password } = req.body;
 
-        // Check if the user exists in the database
-        const user = await db.query(
-            "SELECT * FROM users WHERE username = '" + username + "' AND password = '" + password + "'"
+        // Fetch the user from the database based on the username
+        const [user] = await db.query(
+            "SELECT * FROM users WHERE username = ?",
+            [username]
         );
 
-        if (user.length === 1) {
-            res.status(200).send("Login successful");
-        } else {
-            res.status(401).send("Invalid username or password");
+        if (user.length === 0) {
+            // User not found
+            return res.status(401).send("Invalid username or password");
         }
+
+        // Compare the provided password with the stored hashed password
+        const passwordMatch = await bcrypt.compare(password, user[0].password);
+
+        if (!passwordMatch) {
+            // Passwords do not match
+            return res.status(401).send("Invalid username or password");
+        }
+
+        // Authentication successful
+        res.status(200).send("Login successful");
     } catch (error) {
         console.error(error.message);
         res.status(500).send("Error during login");
     }
 });
-
 
 
 app.listen(3000,()=>{
--- db.js
+++ db.js
@@ -1,18 +1,21 @@
-const mysql = require('mysql');
+const mysql = require('mysql2/promise');
 
-const connection = mysql.createConnection({
+
+const dbconfig = {
     host: 'localhost', // MySQL host (e.g., 'localhost')
-    user: 'root', // MySQL username
-    password: 'x2f9h3m6', // MySQL password
+    user: 'user1', // MySQL username
+    password: 'Beaconhouse45', // MySQL password
     database: 'bidnest' // MySQL database name
-});
+};
+const connection = mysql.createConnection(dbconfig);
+const pool = mysql.createPool(dbconfig)
 
-connection.connect((err) => {
-    if (err) {
-        console.error('Error connecting to MySQL:', err);
-        return;
-    }
-    console.log('Connected to MySQL database!');
-});
+// connection.connect((err) => {
+//     if (err) {
+//         console.error('Error connecting to MySQL:', err);
+//         return;
+//     }
+//     console.log('Connected to MySQL database!');
+// });
 
-module.exports = connection;
+module.exports = pool;
