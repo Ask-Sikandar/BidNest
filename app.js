f6ea3993d7235b2acd74129349522112f494c254
Sikandar Ali <sikandar.6a@gmail.com>
2023-11-27 00:00:24 +0500

    Added create, edit and search properties apis

--- app.js
+++ app.js
@@ -60,27 +60,87 @@ app.post("/login", async (req, res) => {
 });
 
 
-app.post('/create_listing', async (req, res) => {
-    const data = req.body;
-    const token = data[token];
-    console.log(token);
-  
-    // Check if all required fields are present in the request
-    const requiredFields = ['name', 'description', 'bedrooms', 'bathrooms', 'location', 'starting_bid', 'end_time', 'user_username'];
-    if (!requiredFields.every(field => data[field])) {
-      return res.status(400).json({ error: 'Missing required fields' });
-    }
-  
-    // Create a new property listing
-    const newListing = await db.query(
-        "Insert into properties" 
-    );
-  
-    // Add the new listing to the in-memory storage
-    propertyListings.push(newListing);
-  
-    return res.status(201).json({ message: 'Property listing created successfully' });
+app.post('/create-property', (req, res) => {
+    const token = req.headers.authorization.split('.')[1];
+    jwt.verify(token, 'secret', (err, decoded) => {
+      if (err) {
+        return res.status(401).json({ message: 'Invalid token' });
+      }
+      const property = new Property({
+        name: req.body.name,
+        description: req.body.description,
+        bedrooms: req.body.bedrooms,
+        bathrooms: req.body.bathrooms,
+        location: req.body.location,
+        starting_bid: req.body.starting_bid,
+        end_time: req.body.end_time,
+        user_username: req.body.user_username,
+      });
+      connection.query('INSERT INTO properties SET ?', property, (err, result) => {
+        if (err) {
+          return res.status(500).json({ message: 'Error saving property' });
+        }
+        return res.status(201).json({ message: 'Property created successfully' });
+      });
+    });
   });
+  
+//   app.post('/search-property', (req, res) => {
+//     const token = req.headers.authorization.split(' ')[1];
+//     jwt.verify(token, 'secret', (err, decoded) => {
+//       if (err) {
+//         return res.status(401).json({ message: 'Invalid token' });
+//       }
+//       const filters = req.body;
+//       const query = `SELECT * FROM properties WHERE name LIKE '%${filters.name}%' AND description LIKE '%${filters.description}%' AND bedrooms = ${filters.bedrooms} AND bathrooms = ${filters.bathrooms} AND location LIKE '%${filters.location}%' AND starting_bid >= ${filters.starting_bid} AND end_time <= '${filters.end_time}' AND user_username LIKE '%${filters.user_username}%'`;
+//       connection.query(query, (err, results) => {
+//         if (err) {
+//           return res.status(500).json({ message: 'Error searching properties' });
+//         }
+//         return res.status(200).json({ properties: results });
+//       });
+//     });
+//   });
+
+//   app.put('/edit-property/:propertyID', (req, res) => {
+//     const token = req.headers.authorization.split(' ')[1];
+//     jwt.verify(token, 'secret', (err, decoded) => {
+//       if (err) {
+//         return res.status(401).json({ message: 'Invalid token' });
+//       }
+//       const propertyID = req.params.propertyID;
+//       const filters = req.body;
+//       const query = `SELECT * FROM properties WHERE propertyID = ${propertyID}`;
+//       connection.query(query, (err, results) => {
+//         if (err) {
+//           return res.status(500).json({ message: 'Error searching properties' });
+//         }
+//         if (results.length === 0) {
+//           return res.status(404).json({ message: 'Property not found' });
+//         }
+//         if (results[0].user_username !== decoded.username) {
+//           return res.status(401).json({ message: 'Unauthorized' });
+//         }
+//         const property = new Property({
+//           name: filters.name || results[0].name,
+//           description: filters.description || results[0].description,
+//           bedrooms: filters.bedrooms || results[0].bedrooms,
+//           bathrooms: filters.bathrooms || results[0].bathrooms,
+//           location: filters.location || results[0].location,
+//           starting_bid: filters.starting_bid || results[0].starting_bid,
+//           end_time: filters.end_time || results[0].end_time,
+//           user_username: filters.user_username || results[0].user_username,
+//         });
+//         const updateQuery = `UPDATE properties SET name = '${property.name}', description = '${property.description}', bedrooms = ${property.bedrooms}, bathrooms = ${property.bathrooms}, location = '${property.location}', starting_bid = ${property.starting_bid}, end_time = '${property.end_time}', user_username = '${property.user_username}' WHERE propertyID = ${propertyID}`;
+//         connection.query(updateQuery, (err, result) => {
+//           if (err) {
+//             return res.status(500).json({ message: 'Error updating property' });
+//           }
+//           return res.status(200).json({ message: 'Property updated successfully' });
+//         });
+//       });
+//     });
+//   });
 
 
 app.listen(3000,()=>{
